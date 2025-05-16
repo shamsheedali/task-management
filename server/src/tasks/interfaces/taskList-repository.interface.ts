6f90575dcf9364da6b9331d437fab61b32e63ae8
shamsheedali <shamsheedhopz0786@gmail.com>
2025-05-16 20:44:55 +0530

    feat(server/tasklist) delete taslist

--- server/src/tasks/interfaces/taskList-repository.interface.ts
+++ server/src/tasks/interfaces/taskList-repository.interface.ts
@@ -1,8 +1,11 @@
+import { FilterQuery } from 'mongoose';
 import { IBaseRepository } from '../../common/interfaces/base-repository.interface';
 import { ITaskList } from '../models/taskList.model';
 
 export interface ITaskListRepository extends IBaseRepository<ITaskList> {
   create(taskList: { title: string; userId: string }): Promise<ITaskList>;
   findById(id: string): Promise<ITaskList | null>;
+  findOne(query: FilterQuery<ITaskList>): Promise<ITaskList | null>;
   update(id: string, data: Partial<ITaskList>): Promise<ITaskList | null>;
+  delete(id: string): Promise<ITaskList | null>;
 }
