6d60f111cebaea42c4e02b9699d0dc1db9b01cef
shamsheedali <shamsheedhopz0786@gmail.com>
2025-05-20 12:10:59 +0530

    fix: build error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
